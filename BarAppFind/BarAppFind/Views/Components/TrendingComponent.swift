a75946c989919692ae7dfb081119868cac4123bd
Guilherme <guilhermebcavali@gmail.com>
2023-05-04 14:34:32 -0300

    setup: fix components folder

---
+++ BarAppFind/BarAppFind/Views/Components/TrendingComponent.swift
@@ -0,0 +1,20 @@
+//
+//  TrendingComponent.swift
+//  BarAppFind
+//
+//  Created by Guilherme Borges Cavali on 04/05/23.
+//
+
+import SwiftUI
+
+struct TrendingComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TrendingComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        TrendingComponent()
+    }
+}
