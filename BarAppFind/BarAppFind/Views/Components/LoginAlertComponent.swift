8d7249716de74e257668b9602fb04844413f0b86
Guilherme <guilhermebcavali@gmail.com>
2023-05-16 14:41:04 -0300

    modificado componente

--- BarAppFind/BarAppFind/Views/Components/LoginAlertComponent.swift
+++ BarAppFind/BarAppFind/Views/Components/LoginAlertComponent.swift
@@ -13,62 +13,58 @@ struct LoginAlertComponent: View {
     @Binding var isShow: Bool
     
     var body: some View {
-        VStack {
-            VStack(spacing: 20) {
-                HStack(alignment: .top, spacing: 16) {
-                    Image(systemName: "exclamationmark.circle.fill")
-                    
-                    VStack(alignment: .leading, spacing: 12) {
-                        Text(self.title)
-                            .font(.system(size: 16))
-                        
-                        
-                        Text(self.description)
-                            .font(.system(size: 14))
-                    }
-                }
+        VStack(spacing: 20) {
+            
+            // Textos
+            HStack(alignment: .top, spacing: 16) {
+                Image(systemName: "exclamationmark.circle.fill")
                 
-                // Botões do PopUp
-                HStack {
-                    Button {
-                        self.isShow = false
-                    } label: {
-                        Text("Mais tarde")
-                            .font(.system(size: 14))
-                            .foregroundColor(Color("white"))
-                    }
-                    .padding(.vertical, 5)
-                    .padding(.horizontal, 24)
-                    .background(Color("gray4"))
-                    .cornerRadius(12)
+                VStack(alignment: .leading, spacing: 12) {
+                    Text(self.title)
+                        .font(.system(size: 16))
                     
-                    Spacer()
-                    
-                    Button {
-                    } label: {
-                        Text("Fazer Login")
-                            .font(.system(size: 14))
-                            .foregroundColor(Color("white"))
-                    }
-                    .padding(.vertical, 5)
-                    .padding(.horizontal, 24)
-                    .background(Color("gray1"))
-                    .cornerRadius(12)
-
+                    Text(self.description)
+                        .font(.system(size: 14))
                 }
-
-
-                
             }
-            .padding(.horizontal, 32)
-            .padding(.vertical, 24)
-            .frame(width: UIScreen.main.bounds.width - 48)
-            .background(.white)
-            .cornerRadius(12)
-            .shadow(color: .primary.opacity(0.1), radius: 5, x: 0, y: 4)
-            .animation(.spring())
+            
+            // Botões do PopUp
+            HStack {
+                Button {
+                    self.isShow = false
+                } label: {
+                    Text("Mais tarde")
+                        .font(.system(size: 14))
+                        .foregroundColor(Color("white"))
+                }
+                .padding(.vertical, 5)
+                .padding(.horizontal, 24)
+                .background(Color("gray4"))
+                .cornerRadius(12)
+                
+                Spacer()
+                
+                Button {
+                } label: {
+                    Text("Fazer Login")
+                        .font(.system(size: 14))
+                        .foregroundColor(Color("white"))
+                }
+                .padding(.vertical, 5)
+                .padding(.horizontal, 24)
+                .background(Color("gray1"))
+                .cornerRadius(12)
+
+            }
         }
-        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        .padding(.horizontal, 32)
+        .padding(.vertical, 24)
+        .frame(width: UIScreen.main.bounds.width - 48)
+        .background(Color("white"))
+        .cornerRadius(12)
+        .shadow(color: .primary.opacity(0.1), radius: 5, x: 0, y: 4)
+        .animation(.spring())
+        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - 20)
         .background(.secondary.opacity(0.6))
         .offset(y: isShow ? -10 : UIScreen.main.bounds.height)
     }
