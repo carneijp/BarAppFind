06471993e02a44da56216412606562fbc50ea801
Guilherme <guilhermebcavali@gmail.com>
2023-05-18 16:53:36 -0300

    popup das medalhas

--- BarAppFind/BarAppFind/Data/Objects/City.swift
+++ BarAppFind/BarAppFind/Data/Objects/City.swift
@@ -117,10 +117,12 @@ class Review: ObservableObject, Hashable, Identifiable {
 }
 
 var trendings: [String] = ["trending1","trending2","trending3"]
-var moodsImage: [String] = ["Family friendly","Fofoca","Barzinho","Esquenta","Role de casal","Ambientes temáticos"]
-var moodsName: [String] = ["Family friendly","Fofoca","Barzinho","Esquenta","Role de casal","Ambientes temáticos"]
-var moodsBanner: [String] = ["Family friendly2","Fofoca2","Barzinho2","Esquenta2","Role de casal2","Ambientes temáticos2"]
-var moodsColors: [String] = ["Family friendly3","Fofoca3","Barzinho3","Esquenta3","Role de casal3","Ambientes temáticos3"]
+var moodsImage: [String] = ["Família","Fofoca","Barzinho","Esquenta","Role de casal","Temático"]
+var moodsName: [String] = ["Família","Fofoca","Barzinho","Esquenta","Role de casal","Temático"]
+var moodsBanner: [String] = ["Família2","Fofoca2","Barzinho2","Esquenta2","Role de casal2","Temático2"]
+var moodsColors: [String] = ["Família3","Fofoca3","Barzinho3","Esquenta3","Role de casal3","Temático3"]
 var conquestMedals: [String] = ["Bom finer", "Já sabe do que gosta", "Nível 2", "Nível 3", "Nível 4", "Nível 5", "Nível 6"]
+var medalDescriptions: [String] = ["Você conhece todos os bares do bairro Bom Fim! 🤩", "Você já definiu o seu gosto dentro do app🤩"]
+
 
 
--- BarAppFind/BarAppFind/Views/Components/LoginAlertComponent.swift
+++ BarAppFind/BarAppFind/Views/Components/LoginAlertComponent.swift
@@ -24,6 +24,7 @@ struct LoginAlertComponent: View {
                 VStack(alignment: .leading, spacing: 12) {
                     Text(self.title)
                         .font(.system(size: 16))
+                        .bold()
                     
                     Text(self.description)
                         .font(.system(size: 14))
