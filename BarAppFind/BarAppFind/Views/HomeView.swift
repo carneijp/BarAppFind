65f3f47f35977cac981b722db5b06e06a8d26dc5
Guilherme <guilhermebcavali@gmail.com>
2023-05-19 17:12:34 -0300

    sefari party

--- BarAppFind/BarAppFind/Views/HomeView.swift
+++ BarAppFind/BarAppFind/Views/HomeView.swift
@@ -14,6 +14,7 @@ struct HomeView: View {
     @State private var showSignInList: Bool = false
     @State private var viewIndex: Int = 0
     private let carouselTimer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+    @State private var showSerafini: Bool = false
 
     
     var body: some View {
@@ -128,8 +129,13 @@ struct HomeView: View {
                     Spacer()
                 }
             }
+            .onAppear() {
+                showSerafini = true
+            }
             
             LoginAlertComponent(title: "Login Necessário!", description: "Para favoritar bares, realize o seu login!", isShow: $showSignIn)
+            
+            SerafiniComponent(isShow: $showSerafini)
 
         }
         .onAppear() {
