b5e9dbc28c0315753ddf67b3c7ab581e97c7ae83
Eduardo Menegaz Pretto <eduardo.pretto@edu.pucrs.br>
2023-05-24 17:21:59 -0300

    mudando perfil

--- BarAppFind/BarAppFind/Views/Components/EditProfileComponent.swift
+++ BarAppFind/BarAppFind/Views/Components/EditProfileComponent.swift
@@ -8,13 +8,64 @@
 import SwiftUI
 
 struct EditProfileComponent: View {
+    @EnvironmentObject private var cloud: CloudKitCRUD
+    @Environment(\.presentationMode) var presentation
+//    var cliente: Clients
+    @State var firstName: String
+    @State var lastName: String
+    @State var email: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                Text("Faça o seu Login")
+                    .font(.system(size: 16))
+                    .bold()
+                    .padding(.leading, UIScreen.main.bounds.width/3.7)
+                //                .background(.blue)
+                
+                Spacer()
+                
+                Button {
+                    presentation.wrappedValue.dismiss()
+                } label: {
+                    Image(systemName: "x.circle")
+                }
+            }
+            .padding(.horizontal, 24)
+            .padding(.vertical, 12)
+            .background(.secondary.opacity(0.05))
+            .padding(.bottom, 30)
+            
+            TextField("i", text: $firstName, axis: .vertical)
+                .lineLimit(1, reservesSpace: true)
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 10).fill(Color("gray0")))
+                .foregroundColor(.primary)
+                .padding(.horizontal, 24)
+            
+            TextField("", text: $firstName, axis: .vertical)
+                .lineLimit(1, reservesSpace: true)
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 10).fill(Color("gray0")))
+                .foregroundColor(.primary)
+                .padding(.horizontal, 24)
+        
+            TextField("", text: $firstName, axis: .vertical)
+                .lineLimit(1, reservesSpace: true)
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 10).fill(Color("gray0")))
+                .foregroundColor(.primary)
+                .padding(.horizontal, 24)
+        }
+        .onAppear(){
+            
+        }
     }
 }
 
-struct EditProfileComponent_Previews: PreviewProvider {
-    static var previews: some View {
-        EditProfileComponent()
-    }
-}
+//struct EditProfileComponent_Previews: PreviewProvider {
+//    static var previews: some View {
+//        EditProfileComponent()
+//    }
+//}
