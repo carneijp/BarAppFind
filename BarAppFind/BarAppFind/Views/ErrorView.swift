a837a0a43a13ec764b3414ba017ff0489a31e327
IsaBrendler <brendler22@gmail.com>
2023-05-25 14:40:05 -0300

    pagina de erro

---
+++ BarAppFind/BarAppFind/Views/ErrorView.swift
@@ -0,0 +1,47 @@
+//
+//  ErrorView.swift
+//  Onde
+//
+//  Created by Isadora Brasil L. Brendler on 25/05/23.
+//
+
+//import Foundation
+import SwiftUI
+ 
+struct ErrorView: View {
+    var body: some View {
+        NavigationStack{
+            VStack{
+                LogoComponent()
+                    .padding(.top, 20)
+                Spacer ()
+                Image ("erro404")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250, alignment:.center)
+                
+                Text ("Parece que tivemos um problema")
+                    .foregroundColor(.gray)
+                    .font(.system(size: 20))
+                    .bold()
+                    .padding(.top)
+                Text ("Verifique sua conexão com a Internet")
+                    .foregroundColor(.gray)
+                Spacer ()
+                
+                NavigationLink (destination: EmptyView(), label: { Text ("Tentar novamente") })
+//                    .underline()
+                    .foregroundColor(.purple)
+                    .underline()
+                    
+                Spacer()
+            }
+            .padding(.horizontal,24)
+        }
+    }
+}
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+       ErrorView()
+    }
+}
