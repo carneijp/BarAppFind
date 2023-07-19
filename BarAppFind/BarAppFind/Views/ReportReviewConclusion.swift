160aa91e46ecf69bd6bc9e9e52d7f4dac2fc98ce
carneijp <carneijp@hotmail.com>
2023-07-19 13:56:52 -0300

    add: correcao tela final report

--- BarAppFind/BarAppFind/Views/ReportReviewConclusion.swift
+++ BarAppFind/BarAppFind/Views/ReportReviewConclusion.swift
@@ -22,17 +22,30 @@ struct ReportReviewConclusion: View {
                 .resizable()
                 .scaledToFit()
                 .frame(width: 126, height: 178)
+                .padding(.bottom, 12)
             Text("Obrigado por nos informar")
+                .padding(.vertical, 8)
             Text("Seu feedback é muito importante para mantermos nosso app seguro.")
                 .foregroundColor(.gray)
                 .multilineTextAlignment(.center)
+                .padding(.horizontal, 20)
             
             Button {
                 showSheet = false
                 presentation.wrappedValue.dismiss()
             }label: {
-                Text("OK")
+                HStack{
+                    Spacer()
+                    Text("Ok")
+                        .padding(.vertical, 12)
+                        .padding(.horizontal)
+                    Spacer()
+                }
+                .background(.white)
+                .cornerRadius(24)
+                .shadow(color: Color("gray6") ,radius: 3, x: 0, y: 2)
             }
+            .padding()
             
             Spacer()
         }
